7820cdbf36162abcc0ca8df7a326f8a00c2b59ed
dananjaya <asidananjaya123@gmail.com>
2024-09-05 21:46:58 +0530

    code comment

--- Service/Common/HashPassword.cs
+++ Service/Common/HashPassword.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,20 @@ namespace car_traders.Service.Common
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                // ComputeHash returns byte array
+                // converts the password (a string) into bytes using UTF-8 encoding and then hashes it.
+                //this produces a byte array as the hashed result.
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
 
-                // Convert byte array to a string
+                // StringBuilder to build the hashed password as a readable string
                 StringBuilder builder = new StringBuilder();
+                // loop byte  hashed result
                 for (int i = 0; i < bytes.Length; i++)
                 {
+                    //Each byte is converted to a hexadecimal string
                     builder.Append(bytes[i].ToString("x2"));
                 }
                 return builder.ToString();
+                //returns the complete hashed password
             }
         }
     }
